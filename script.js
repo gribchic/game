06004cf1967b770fc439f5fd384ea662633bfa2e
gribchic <andrey_kosht@mail.ru>
2019-05-28 01:27:00 +0300

    test2
    add jpgs;
    add start menu;
    add records;
    imgs hide;

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 'use strict';
 
 function Game() {
-
 	let ar = [],
 	current = {},
-	timer = {},
+	timer,
 	counter,
 	gameField = document.getElementById('gameField'),
+	recordsField = document.getElementById('recordsField'),
+	startMenu = document.getElementById('startMenu'),
 	records = {},
 	gameRecords = [],
 	size,
@@ -18,19 +19,52 @@ function Game() {
 		counter = 0;
 		setGame();
 		createField();
-		timer.start = new Date;
+		timer = new Date;
+		startMenu.style.height = 0;
+	}
+
+	this.showRecords = function () {
+		let length = gameRecords.length,
+		list = document.createElement('ul'),
+		li = createLi('#', 'user', 'score');
+
+		list.appendChild(li);
+
+		for (let i = 0; i < length; i++) {
+			let ind = i > 9 ? '0' + (i + 1) : i + 1;
+
+			li = createLi(ind, gameRecords[i].user, gameRecords[i].score);//`<span>${ind}</span> <span>${gameRecords[i].user}</span> <span>${gameRecords[i].score}</span>`;
+			if (gameRecords[i].user == user)
+				li.style.fontWeight = '700';
+			list.appendChild(li);
+		};
+
+		showField(recordsField, list)
+	}
+
+	function createLi(ind, user, score) {
+		let li = document.createElement('li');
+		
+		li.innerHTML =`<span>${ind}</span> <span>${user}</span> <span>${score}</span>`;
+		return li;
+	}
+
+	function showField(parentEl, children) {
+		parentEl.innerHTML = null;
+		parentEl.appendChild(children);
 	}
 
 	function endGame() {
-		let score = (new Date) - timer.start;
+		let score = (new Date) - timer;
 		setRecords(score);
+		startMenu.style.height = 'auto';
 	}
 
 	function setRecords(score) {
 		let length = gameRecords.length,
-			maxLength = 3;
-		
-		if (length == maxLength &&  gameRecords[length].score <= score) {
+		maxLength = 10;
+
+		if (length == maxLength && gameRecords[length].score <= score) {
 			return;
 		}
 
@@ -44,37 +78,49 @@ function Game() {
 		if (gameRecords.length > maxLength) {
 			gameRecords.pop();
 		}
-		
+
 		gameRecords.some(
-			(item, index, array)=>{
-				
-				if(item.user == user && item.score == score) {
-					alert(`Congratulations ${user}! Your place: ${index+1}, Your score: ${score}`);
-				}
-				return false;
+			(item, index, array) => {
+
+			if (item.user == user && item.score == score) {
+				alert(`Congratulations ${user}! Your place: ${index+1}, Your score: ${score}`);
 			}
-		);
+			return false;
+		});
 	}
 
 	function createField() {
 		const table = document.createElement('table');
+		let rows='';
 
 		for (let i = 0; i < size; i++) {
-			let row = document.createElement('tr')
+			let tds = '';
 
 				for (let j = 0; j < size; j++) {
-					let cell = document.createElement('td');
-					let img = document.createElement('img')
-						img.src = 'img/' + ar[i][j] + '.jpg';
+					
+					//let flipper = `<div class="flip-container" data-img="${ar[i][j]}">
+					//	<div class="flipper">
+					//		<div class="front"></div>
+					//		<div class="back"><img src="img/${ar[i][j]}.jpg"></div>
+					//	 </div>
+					//</div>`;
+					
+					//let cell = document.createElement('td'),
+					//img = document.createElement('img');
 
-					img.dataset.img = ar[i][j];
-					img.style.filter = 'contrast(0)';
-					cell.id = ar[i][j];
-					cell.appendChild(img);
-					row.appendChild(cell);
+					let img = `<img src="img/${ar[i][j]}.jpg" data-img="${ar[i][j]}" style="filter:contrast(0)">`;
+					
+						tds += `<td>${img}</td>`;
+					//img.dataset.img = ar[i][j];
+					//img.style.filter = 'contrast(0)';
+					//cell.appendChild(img);
+					//cell.innerHTML = flipper;
+					
 				}
-				table.appendChild(row);
+				rows += `<tr>${tds}</tr>`;
+				//table.appendChild(row);
 		}
+		table.innerHTML = rows;
 
 		table.addEventListener('click', openImg);
 
@@ -87,36 +133,74 @@ function Game() {
 	}
 
 	function openImg(e) {
-
-		console.log('click');
-		let img = e.target.nodeName === 'IMG' ? e.target : null;
+		//let target = e.target.className === 'front' ? e.target : null,
+		//	table = e.currentTarget;
+		//if (!target) {
+		//	return;
+		//};
+		//let cell = target.parentNode.parentNode;
+		//
+		//if (!current.cell) {
+		//	cell.classList.add('open');
+		//	current.cell = cell;
+		//	return;
+		//} else if (current.cell.dataset.img !== cell.dataset.img) {
+		//	cell.classList.remove('open');
+		//	current.cell.classList.remove('open');
+		//	openSecondImg(table, cell);
+		//} else {
+		//	counter++;
+		//	isAllImagesOpened() ? openSecondImg(table, cell, endGame) : openSecondImg(table, cell);
+		//};
+		//delete current.cell;
+		
+		////////////////////////////////////////////
+		let img = e.target.nodeName === 'IMG' ? e.target : null,
+		table = e.currentTarget,
+		currentImg = current.img;
+		
 		if (!img || !!img.dataset.isOpen) {
 			return;
 		}
 		img.style.filter = 'contrast(1)';
 		img.dataset.isOpen = true;
-
+		
 		if (!current.img) {
 			current.img = img;
 			return;
 		} else if (current.img.dataset.img !== img.dataset.img) {
 			delete img.dataset.isOpen;
 			delete current.img.dataset.isOpen;
-			let currentImg = current.img,
-			table = e.currentTarget;
-			table.addEventListener('click', stopPropagation, true);
-			setTimeout(function () {
-				img.style.filter = currentImg.style.filter = 'contrast(0)';
-				table.removeEventListener('click', stopPropagation, true);
-			}, 500);
-
+			openSecondImg(table, img, currentImg, 'contrast(0)');
 		} else {
 			counter++;
+			isAllImagesOpened() ? openSecondImg(table, img, currentImg, 'opacity(0)', endGame) : openSecondImg(table, img, currentImg, 'opacity(0)');
 		};
 		delete current.img;
-		if (counter >= size * size / 2) {
-			endGame();
-		}
+	}
+
+	function isAllImagesOpened() {
+		return counter >= size * size / 2;
+	}
+	
+	//function openSecondImg(table, cell, func) {
+	//	table.addEventListener('click', stopPropagation, true);
+	//	setTimeout(() => {
+	//		cell.classList.add('open');
+	//		table.removeEventListener('click', stopPropagation, true);
+	//		if (func)
+	//			func();
+	//	}, 500);
+	//}
+
+	function openSecondImg(table, img, currentImg, filter, func) {
+		table.addEventListener('click', stopPropagation, true);
+		setTimeout(() => {
+			img.style.filter = currentImg.style.filter = filter;
+			table.removeEventListener('click', stopPropagation, true);
+			if (func)
+				func();
+		}, 500);
 	}
 
 	function setGame() {
@@ -127,9 +211,7 @@ function Game() {
 		};
 
 		arr2 = arr2.concat(arr2);
-		arr2.sort(function () {
-			return Math.random() - 0.5;
-		});
+		arr2.sort(() => Math.random() - 0.5);
 
 		for (var i = 0; i < size; i++) {
 			ar[i] = arr2.splice(0, size)
@@ -150,4 +232,7 @@ function startGame() {
 	}
 
 	game.start(size, user);
+}
+function showRecords() {
+	game.showRecords();
 }
